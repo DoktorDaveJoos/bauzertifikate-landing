a431c6e49560cebd038f312301fe65655663b187
David Joos <drdavejoos@gmail.com>
2024-03-05 14:34:27 +0100

    WIP fix contact

--- components/landing/Contactform.vue
+++ components/landing/Contactform.vue
@@ -60,7 +60,7 @@ onMounted(() => {
     class="needs-validation"
     novalidate
   >
-    <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+    <input type="hidden" name="access_key" value="ca86543a-a428-4dd4-b1f5-c3bcebd670b1" />
     <!-- Create your free access key from https://web3forms.com/ -->
     <input
       type="checkbox"
@@ -71,44 +71,44 @@ onMounted(() => {
     <div class="mb-5">
       <input
         type="text"
-        placeholder="Full Name"
+        placeholder="Name"
         required
         class="w-full px-4 py-3 border-2 placeholder:text-gray-800 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
         name="name"
       />
       <div class="empty-feedback invalid-feedback text-red-400 text-sm mt-1">
-        Please provide your full name.
+        Geben Sie Ihren Namen ein.
       </div>
     </div>
     <div class="mb-5">
-      <label for="email_address" class="sr-only">Email Address</label
+      <label for="email_address" class="sr-only">Email-Adresse</label
       ><input
         id="email_address"
         type="email"
-        placeholder="Email Address"
+        placeholder="Email-Adresse"
         name="email"
         required
         class="w-full px-4 py-3 border-2 placeholder:text-gray-800 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
       />
       <div class="empty-feedback text-red-400 text-sm mt-1">
-        Please provide your email address.
+        Bitte geben Sie Ihre E-Mail-Adresse an.
       </div>
       <div class="invalid-feedback text-red-400 text-sm mt-1">
-        Please provide a valid email address.
+        Bitte geben Sie eine gültige E-Mail-Adresse an.
       </div>
     </div>
     <div class="mb-3">
       <textarea
         name="message"
         required
-        placeholder="Your Message"
+        placeholder="Deine Nachricht"
         class="w-full px-4 py-3 border-2 placeholder:text-gray-800 rounded-md outline-none h-36 focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
       ></textarea>
       <div class="empty-feedback invalid-feedback text-red-400 text-sm mt-1">
-        Please enter your message.
+        Bitte geben Sie Ihre Nachricht ein.
       </div>
     </div>
-    <LandingButton type="submit" size="lg" block>Send Message</LandingButton>
+    <LandingButton type="submit" size="lg" block>Abschicken</LandingButton>
     <div id="result" class="mt-3 text-center"></div>
   </form>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -7,28 +7,37 @@ definePageMeta({
 <template>
   <LandingContainer>
     <LandingSectionhead>
-      <template v-slot:title>Contact</template>
-      <template v-slot:desc>I am here to help.</template>
+      <template v-slot:title>Kontakt</template>
+      <template v-slot:desc>Ob Partnerschaft, Kontakt oder sonstige Nachfragen.</template>
     </LandingSectionhead>
 
     <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl mt-16">
       <div>
         <h2 class="font-medium text-2xl text-gray-800">
-          Contact Nuxtship Creator
+          Kontaktieren Sie Uns.
         </h2>
         <p class="text-lg leading-relaxed text-slate-500 mt-3">
-          Have something to say? I am here to help. Fill up the form or send
-          email.
+          Sie haben Fragen oder benötigen Unterstützung? Ob Partnerschaftsanfrage, Demoanfrage oder allgemeine Anfrage –
+          wir sind für Sie da! Füllen Sie einfach das untenstehende Formular aus, und wir werden uns innerhalb von 48
+          Stunden bei Ihnen melden, um Ihre Anliegen zu besprechen.
         </p>
         <div class="mt-5">
           <div class="flex items-center mt-2 space-x-2 text-gray-600">
-            <Icon class="text-gray-400 w-4 h-4" name="uil:map-marker" />
-            <span>1010 Vienna, Austria</span>
+            <Icon class="text-gray-400 w-4 h-4" name="uil:map-marker"/>
+            <span>Marktstraße 41, Ravensburg</span>
           </div>
           <div class="flex items-center mt-2 space-x-2 text-gray-600">
-            <Icon class="text-gray-400 w-4 h-4" name="uil:envelope" /><a
-              href="mailto:jakobaichmayr@gmail.com"
-              >jakobaichmayr@gmail.com</a
+            <Icon class="text-gray-400 w-4 h-4" name="uil:envelope"/>
+            <a
+                href="mailto:hannes@bauzertifikate.de"
+            >hannes@bauzertifikate.de</a
+            >
+          </div>
+          <div class="flex items-center mt-2 space-x-2 text-gray-600">
+            <Icon class="text-gray-400 w-4 h-4" name="uil:envelope"/>
+            <a
+                href="mailto:david@bauzertifikate.de"
+            >david@bauzertifikate.de</a
             >
           </div>
           <!-- <div class="flex items-center mt-2 space-x-2 text-gray-600">
@@ -40,7 +49,7 @@ definePageMeta({
         </div>
       </div>
       <div>
-        <LandingContactform />
+        <LandingContactform/>
       </div>
     </div>
   </LandingContainer>
